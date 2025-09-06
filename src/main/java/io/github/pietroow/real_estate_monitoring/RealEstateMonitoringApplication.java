567764aa43baaffafd409d22037c5afffa11fde2
SanteLGG <l.g.g.sante@gmail.com>
2025-09-05 22:26:48 -0300

    Creating the project real-estate-monitoring with Java 21 and SpringBoot 3.5.5 - First Commit

---
+++ src/main/java/io/github/pietroow/real_estate_monitoring/RealEstateMonitoringApplication.java
@@ -0,0 +1,13 @@
+package io.github.pietroow.real_estate_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pietroow/real_estate_monitoring/RealEstateMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pietroow.real_estate_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
