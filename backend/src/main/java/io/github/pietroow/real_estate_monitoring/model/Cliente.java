eed269c586a444207f77ecf6a08a0282372df1a8
Nikolas Brendo <nbrendo29@gmail.com>
2025-09-29 22:44:36 -0300

    Algumas correções e implementação de algumas funções

--- backend/src/main/java/io/github/pietroow/real_estate_monitoring/model/Cliente.java
+++ backend/src/main/java/io/github/pietroow/real_estate_monitoring/model/Cliente.java
@@ -5,56 +5,54 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="cliente")
+@Table(name = "cliente")
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    //Informações de Pessoa Jurídica
-    @Column(name="nome")
+    @Column(name = "nome")
     private String nome;
 
     @NotNull
-    @Column(name="razao_social")
+    @Column(name = "razao_social")
     private String razaoSocial;
 
-
     @Enumerated(EnumType.STRING)
-    @Column(name="tipo")
+    @Column(name = "tipo")
     private TipoPessoa tipo;
 
-    @Column(name="cpf_cnpj",unique = true)
+    @Column(name = "cpf_cnpj", unique = true)
     private String cpfCnpj;
 
-    @Column(name="inscricao_estadual")
+    @Column(name = "inscricao_estadual")
     private String inscricaoEstadual;
 
-    @Column(name="inscricao_municipal")
+    @Column(name = "inscricao_municipal")
     private String inscricaoMunicipal;
 
-    @Column(name="telefone_1")
+    @Column(name = "telefone_1")
     private String telefone1;
 
-    @Column(name="telefone_2")
+    @Column(name = "telefone_2")
     private String telefone2;
 
-    @Column(name="email")
+    @Column(name = "email")
     private String email;
 
     @Column(columnDefinition = "TEXT")
     private String comentario;
 
-
-    //Comum entre PF e PJ
-    @ManyToOne
-    Endereco endereco;
+    @OneToOne(mappedBy = "endereco", fetch = FetchType.LAZY)
+    private Endereco endereco;
 }
--- backend/src/main/java/io/github/pietroow/real_estate_monitoring/model/Endereco.java
+++ backend/src/main/java/io/github/pietroow/real_estate_monitoring/model/Endereco.java
@@ -1,21 +1,43 @@
 package io.github.pietroow.real_estate_monitoring.model;
 
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "endereco")
 public class Endereco {
 
-    String cep;
-    String endereco;
-    String numero;
-    String complemento;
-    String bairro;
-    String estado;
-    String cidade;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-    }
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column
+    private String numero;
+
+    @Column
+    private String complemento;
+
+    @Column
+    private String bairro;
+
+    @Column
+    private String estado;
+
+    @Column
+    private String cidade;
+}
