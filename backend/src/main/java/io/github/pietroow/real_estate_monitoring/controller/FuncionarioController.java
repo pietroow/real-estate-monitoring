a133c831a791fc5939f23c2f4054d37f58c89ca4
Vinicius Simoes Campos <viniciussimoescampos@gmail.com>
2025-10-17 18:04:41 -0300

    Aplicando correções

--- backend/src/main/java/io/github/pietroow/real_estate_monitoring/controller/FuncionarioController.java
+++ backend/src/main/java/io/github/pietroow/real_estate_monitoring/controller/FuncionarioController.java
@@ -2,16 +2,16 @@ package io.github.pietroow.real_estate_monitoring.controller;
 
 import io.github.pietroow.real_estate_monitoring.dto.FuncionarioRequestDTO;
 import io.github.pietroow.real_estate_monitoring.dto.FuncionarioResponseDTO;
+import io.github.pietroow.real_estate_monitoring.mapper.FuncionarioMapper;
+import io.github.pietroow.real_estate_monitoring.model.Funcionario;
 import io.github.pietroow.real_estate_monitoring.service.FuncionarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -20,40 +20,36 @@ import java.util.UUID;
 public class FuncionarioController {
 
     private final FuncionarioService funcionarioService;
+    private final FuncionarioMapper funcionarioMapper;
 
     @PostMapping
-    public ResponseEntity<FuncionarioResponseDTO> create(@RequestBody FuncionarioRequestDTO requestDTO) {
-        FuncionarioResponseDTO responseDTO = funcionarioService.create(requestDTO);
-        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public FuncionarioResponseDTO create(@RequestBody @Valid FuncionarioRequestDTO requestDTO) {
+        Funcionario novoFuncionario = funcionarioService.create(requestDTO);
+        return funcionarioMapper.toDto(novoFuncionario);
     }
 
     @GetMapping
-    public ResponseEntity<Page<FuncionarioResponseDTO>> findAll(Pageable pageable) {
-        Page<FuncionarioResponseDTO> page = funcionarioService.findAll(pageable);
-        return new ResponseEntity<>(page, HttpStatus.OK);
+    public Page<FuncionarioResponseDTO> findAll(Pageable pageable) {
+        Page<Funcionario> pageDeFuncionarios = funcionarioService.findAll(pageable);
+        return pageDeFuncionarios.map(funcionarioMapper::toDto);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<FuncionarioResponseDTO> findById(@PathVariable UUID id) {
-        Optional<FuncionarioResponseDTO> responseDTO = funcionarioService.findById(id);
-        return responseDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public FuncionarioResponseDTO findById(@PathVariable UUID id) {
+        Funcionario funcionario = funcionarioService.findById(id);
+        return funcionarioMapper.toDto(funcionario);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<FuncionarioResponseDTO> update(@PathVariable UUID id, @RequestBody FuncionarioRequestDTO requestDTO) {
-        Optional<FuncionarioResponseDTO> responseDTO = funcionarioService.update(id, requestDTO);
-        return responseDTO.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public FuncionarioResponseDTO update(@PathVariable UUID id, @RequestBody @Valid FuncionarioRequestDTO requestDTO) {
+        Funcionario funcionarioAtualizado = funcionarioService.update(id, requestDTO);
+        return funcionarioMapper.toDto(funcionarioAtualizado);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable UUID id) {
-        try {
-            funcionarioService.delete(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (EmptyResultDataAccessException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID id) {
+        funcionarioService.delete(id);
     }
 }
